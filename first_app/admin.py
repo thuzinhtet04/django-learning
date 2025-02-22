a7927beee4aec9d61739e4f74b73833d9cfbaad4
Thu Zin Htet <thuzinhtet.dev@gmail.com>
2025-02-22 14:23:57 +0630

    admin dashboard and register models for dashboard

--- first_app/admin.py
+++ first_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import ToDoList, Item
 
 # Register your models here.
+admin.site.register(ToDoList)
+admin.site.register(Item)
+
+# run command "python manage.py createsuperuser" to create admin acc
